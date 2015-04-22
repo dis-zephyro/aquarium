5e05018709dff839e446bfdd69e960fc3a0e23f5
dis-zephyro <zephyro@yandex.ru>
2015-04-23 01:21:43 +0300

    new

--- js/main.js
+++ js/main.js
@@ -14,4 +14,26 @@ $('.reply-more').click(function() {
     $('.reply').slick('slickNext');
 });
 
+$(".popup").fancybox({
+    'padding' : 0,
+    'closeBtn' : false
+});
 
+$('.btn-close').click(function(){
+    $.fancybox.close();
+});
+
+
+$('.tabs').tabs();
+
+
+$(function() {
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 50) {
+            $('.topnav').addClass('bg');
+        } else {
+            $('.topnav').removeClass('bg');
+        }
+    });
+});
